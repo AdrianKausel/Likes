16d9b73c4728fb4095cb270bc8e24804946aa510
AdrianKausel <103789648+AdrianKausel@users.noreply.github.com>
2022-06-28 00:32:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// ---- FIRST BOX LIKES----
+var likes = 9;
+var addlikes = document.querySelector(".likes");
+
+function addlike1(){
+    likes++;
+    addlikes.innerText = likes + "like(s)";
+}
+// ---- SECOND BOX LIKES----
+var likes2 = 12;
+var addlikes2 = document.querySelector(".likes2");
+
+function addlike2(){
+    likes2++;
+    addlikes2.innerText = likes2 + "like(s)";
+}
+
+// ---- THIRD BOX LIKES----
+
+var likes3 = 9;
+var addlikes3 = document.querySelector(".likes3");
+
+function addlike3(){
+    likes3++;
+    addlikes3.innerText = likes3 + "like(s)";
+}
+
